0f764f082175e1622c14d6c849dcb8a9e1922d64
jonathan-durbin <jongdurbin@gmail.com>
2020-02-12 14:41:06 -0500

    Split client, start work on client1, tasks
    Co-authored-by: Alec Hussak <mr.ahussak@gmail.com>

--- client/client.jl
+++ client/client.jl
@@ -6,53 +6,60 @@ using .Select
 include("../julia/raytracer.jl")
 using .RayTracer
 
-#=
-create a bucket of work that needs to be filled.
-divide up the work between servers defined in SERVER_LIST
-send work to each server
-while waiting, do own work
-if data is sent back, add it to the final image
-=#
 
+# version 1
+function handle(socket, server, file, numbytes, chunks)
+    states = [
+        "waiting for file",
+        "waiting for job",
+        "processing job",
+        "finished"
+    ]
+    server_state = states[1]
+    confirm = UInt8[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+    error   = UInt8[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
 
-"""
-Send a file to a server.
+    while server_state == "waiting for file"
+        open(file, "r") do io
+            # converts the UInt64 hash to an array of UInt8 partial hashes.
+            h = reinterpret(UInt8, [hash(read(io, String))])
+            seekstart(io)
 
-`socket` is a UDPSocket bound to the client ip address
-`server` is a single tuple containing an ip address and a port number
-"""
-function sendfile(
-    socket::UDPSocket,
-    server_list::Array{Tuple{IPv4,Int64},1},
-    file::String,
-    numbytes::Int = 500
-    )
-    open(file, "r") do io
-        # converts the UInt64 hash to an array of UInt8 partial hashes.
-        h = reinterpret(UInt8, [hash(read(io, String))])
-        seekstart(io)
-
-        # the server will have to know to connect the first 8 bytes of file-sending messages to form the file hash
-        bs = read(io, numbytes)
-        for server in server_list
+            # the server will have to know to connect the first 8 bytes of file-sending messages to form the file hash
+            bs = read(io, numbytes)
             while length(bs) > 0
                 # send 500-byte packets, adding the file hash (8 bytes) to the beginning of each message
                 send(socket, server[1], server[2], cat(h, bs, dims=1))
                 bs = read(io, numbytes)
             end
         end
+        send(socket, server[1], server[2], confirm)
+        r = recv(socket)
+        r == confirm ? server_state = states[2] : println("Error while sending file to $server, retrying...")
+    end
+
+    while server_state == "waiting for job"
+        chunk = take!(chunks)
+        send(socket, server[1], server[2], reinterpret(UInt8, [chunk.start, chunk.stop]))
+
+        r = recv(socket)
+        r == confirm ? server_state = states[3] : println("Error while sending job to $server, retrying...")
     end
 end
 
 
-function sendjob(socket::UDPSocket, server::Tuple{IPv4,Int64}, r::UnitRange)
-    send(socket, server[1], server[2], reinterpret(UInt8, [r.start, r.stop]))
+function timeout(n)
+    sleep(n)
+    println("Timed out")
+    return false
 end
 
 
 function main()
+    socket = UDPSocket()
+    bind(socket, ip"127.0.0.1", 9055)
     server_list = [
-        (ip"127.0.0.1", 3000)
+        (ip"127.0.0.1", 5055)
     ]
     file = "client/default.scene"
     numbytes = 500  # Max bytes to send via UDP
@@ -61,26 +68,21 @@ function main()
     view, scene = parseFile(file)
     N = view.width * view.height  # Total number of pixels in image
 
-    tracking = Dict()  # Tracks pixel chunks waiting for from servers
-    for (i, chunk) in enumerate([i+1:i+numpixels for i in 0:numpixels:N])
-        tracking[chunk] = [server_list[i % length(server_list)], 0]  # [server, waittime]
-    end
-    @show tracking
-
-    socket = UDPSocket()
-    bind(socket, ip"127.0.0.1", 3001)
-
-    sf = @async sendfile(socket, server_list, file, 500)
-    sj = @async sendjob()
-    # r  = @async recvfrom(socket)
-    while true
-        @select begin
-            sf => begin
-                println("Sent $file to the server(s)")
-            end
-
+    chunks = Channel{UnitRange{Int64}}(ceil(Int, N/numpixels)) do ch  # Tracks pixel chunks waiting for from servers
+        for chunk in [i+1:i+numpixels for i in 0:numpixels:N]
+            put!(ch, chunk)
         end
     end
+
+    tasks = map(server ->
+        () -> Task(() -> handle(socket, server, file, numbytes, chunks)),
+        server_list
+    )
+    tasklist = [(:take, t()) for t in tasks]
+    while true
+        i, r = select(tasklist)  # i is index of server_list, r is return value of first function to return
+    end
+
     close(socket)
 end
 
---
+++ client/client2.jl
@@ -0,0 +1,81 @@
+using Sockets
+
+include("extra/Select.jl") # From https://github.com/NHDaly/Select.jl/blob/master/src/Select.jl
+using .Select
+
+include("../julia/raytracer.jl")
+using .RayTracer
+
+
+"""
+Send a file to a server.
+
+`socket` is a UDPSocket bound to the client ip address
+`server` is a single tuple containing an ip address and a port number
+"""
+function sendfile(
+    socket::UDPSocket,
+    server_list::Array{Tuple{IPv4,Int64},1},
+    file::String,
+    numbytes::Int = 500
+    )
+    open(file, "r") do io
+        # converts the UInt64 hash to an array of UInt8 partial hashes.
+        h = reinterpret(UInt8, [hash(read(io, String))])
+        seekstart(io)
+
+        # the server will have to know to connect the first 8 bytes of file-sending messages to form the file hash
+        bs = read(io, numbytes)
+        for server in server_list
+            while length(bs) > 0
+                # send 500-byte packets, adding the file hash (8 bytes) to the beginning of each message
+                send(socket, server[1], server[2], cat(h, bs, dims=1))
+                bs = read(io, numbytes)
+            end
+        end
+    end
+end
+
+
+function sendjob(socket::UDPSocket, server::Tuple{IPv4,Int64}, r::UnitRange)
+    send(socket, server[1], server[2], reinterpret(UInt8, [r.start, r.stop]))
+end
+
+
+function main()
+    socket = UDPSocket()
+    bind(socket, ip"127.0.0.1", 9055)
+    server_list = [
+        (ip"127.0.0.1", 5055)
+    ]
+    file = "client/default.scene"
+    numbytes = 500  # Max bytes to send via UDP
+    numpixels = floor(Int, numbytes/3)  # Number of pixels per chunk
+
+    view, scene = parseFile(file)
+    N = view.width * view.height  # Total number of pixels in image
+
+    tracking = Dict()  # Tracks pixel chunks waiting for from servers
+    for (i, chunk) in enumerate([i+1:i+numpixels for i in 0:numpixels:N])
+        tracking[chunk] = [server_list[i % length(server_list)], 0]  # [server, waittime]
+    end
+
+    sf = @async sendfile(socket, server_list, file, 500)
+    sj = @async sendjob()
+    r  = @async recvfrom(socket)
+    while true
+        @select begin
+            sf => begin
+                println("Sent $file to the server(s)")
+            end
+            r =>  begin
+                from, data = r
+                println("Got data from $(from.host): $(String(copy(data)))")
+                r = @async recvfrom(socket)
+            end
+        end
+    end
+    close(socket)
+end
+
+main()
