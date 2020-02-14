0d5eacef16b4bfd2c5e4c38da1aa332e62f8a5a5
jonathan-durbin <jongdurbin@gmail.com>
2020-02-14 14:37:20 -0500

    Create receive_from function

--- client/client.jl
+++ client/client.jl
@@ -7,13 +7,36 @@ include("../julia/raytracer.jl")
 using .RayTracer
 
 
-# version 1
+function receive_from(
+    socket::UDPSocket,
+    expected_server::Tuple{IPv4, Int64},
+    recv_channels::Dict{Tuple{IPv4,Int64},Channel{Any}}
+    )
+    ip, r = recvfrom(socket)
+    ip = (ip.host, ip.port)
+    if ip != expected_server  # if we receive from a server that we haven't been talking to
+        put!(recv_channels[ip], r)
+        r = take!(recv_channels[expected_server])
+        ip = expected_server
+    end
+    return (ip, r)
+end
+
+
+function timeout(n)
+    sleep(n)
+    println("Timed out")
+    return false
+end
+
+
 function handle(
     socket::UDPSocket,
     server::Tuple{IPv4, Int64},
     file::String,
     numbytes::Int,
-    chunks::Channel
+    chunks::Channel,
+    recv_channels::Channel
     )
     states = [
         "waiting for file",
@@ -34,16 +57,13 @@ function handle(
             # the server will have to know to connect the first 8 bytes of file-sending messages to form the file hash
             bs = read(io, numbytes)
             while length(bs) > 0
-                # send 500-byte packets, adding the file hash (8 bytes) to the beginning of each message
+                # send numbytes-byte packets, adding the file hash (8 bytes) to the beginning of each message
                 send(socket, server[1], server[2], cat(h, bs, dims=1))
                 bs = read(io, numbytes)
             end
         end
         send(socket, server[1], server[2], confirm)
-        ip, r = recvfrom(socket)
-        while ip != server[1]
-            ip, r = recvfrom(socket)
-        end
+        ip, r = receive_from(socket, server, recv_channels)
         r == confirm ? server_state = states[2] : println("Error while sending file to $server, retrying...")
     end
 
@@ -51,27 +71,20 @@ function handle(
         chunk = take!(chunks)
         send(socket, server[1], server[2], reinterpret(UInt8, [chunk.start, chunk.stop]))
 
-        ip, r = recvfrom(socket)
-        while ip != server[1]
-            ip, r = recvfrom(socket)
+        ip, r = receive_from(socket, server, recv_channels)
+        if r == confirm
+            server_state = states[3]
+        else
+            put!(chunks, chunk)
+            println("Error while sending job to $server, retrying...")
+            sleep(0.1)
         end
-        r == confirm ? server_state = states[3] : put!(chunks, chunk); println("Error while sending job to $server, retrying..."); sleep(0.1)
     end
 
     while server_state == states[3]  # processing job
-        ip, r = recvfrom(socket)
-        while ip != server[1]
-            ip, r = recvfrom(socket)
-        end
-        if length(r) == chunk.stop - chunk.start  # not chunk, but something similar
+        ip, r = receive_from(socket, server, recv_channels)
+        if length(r) == chunk.stop - chunk.start  # if the length of what we got is shorter/longer than what we expected
     end
-end
-
-
-function timeout(n)
-    sleep(n)
-    println("Timed out")
-    return false
 end
 
 
@@ -94,12 +107,14 @@ function main()
             put!(ch, chunk)
         end
     end
+    recv_channels = Dict(serv => Channel(1) for serv in server_list)
 
     tasks = map(server ->
-        () -> Task(() -> handle(socket, server, file, numbytes, chunks)),
+        () -> Task(() -> handle(socket, server, file, numbytes, chunks, recv_channels)),
         server_list
     )
     tasklist = [(:take, t()) for t in tasks]
+
     while true
         i, r = select(tasklist)  # i is index of server_list, r is return value of first function to return
         # depending on the value of r
