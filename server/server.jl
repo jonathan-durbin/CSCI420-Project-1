33c0f7bceb2e7e4a23f6ebfb61f01e5d170ecaf6
jonathan-durbin <jongdurbin@gmail.com>
2020-02-14 14:37:44 -0500

    Start functionality for receiving jobs

--- server/server.jl
+++ server/server.jl
@@ -9,29 +9,39 @@ bind(sock, ip"127.0.0.1", 5055)
 confirm = UInt8[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
 error   = UInt8[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
 
-scene = ""
+scenein = ""
 h = nothing
 client = ""
 
-while h != hash(scene)
-    global scene, h, client, confirm, error
+# TODO Test if correct client; add temporary bias to receive from previous client
+while h != hash(scenein)
+    global scenein, h, client, confirm, error
 
     client, r = recvfrom(sock)  # recv blocks execution.
     if r == confirm
         send(sock, client, 9055, error)
-        scene = ""
+        scenein = ""
         h = nothing
         client = ""
         continue
     end
     h = reinterpret(UInt64, r[1:8])[1]  # reinterpret combines an array of UInt8 to a single UInt64
-    scene = scene * String(r[9:end])
+    scenein = scenein * String(r[9:end])
 end
 
 send(sock, client, 9055, confirm)
 
 println("Got the whole file!")
-scenebuffer = IOBuffer(scene)
+scenebuffer = IOBuffer(scenein)
+view, scene = parseFile(scenebuffer)
+
+ip = ""
+r = Int64
+while ip != client
+    global ip, r
+    ip, r = recvfrom(sock)
+end
+
 
 
 close(sock)
