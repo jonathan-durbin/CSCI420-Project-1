830ffb02d19583214ddbd0c67d949c2fc442627c
jonathan-durbin <jongdurbin@gmail.com>
2020-02-16 22:10:21 -0500

    Add max number of recv tries,
    also initialize the final image, add to it, check that it's not empty, etc.

--- client/client.jl
+++ client/client.jl
@@ -46,14 +46,8 @@ function receive_from(
         r = take!(recv_channels[expected_server])
         server = expected_server  # r is now the data from the server that we expected, so server can be replaced
     end
+    # FIXME: if above times out, server is now equal to a (probably) unexpected server, so this might not be good
     return (server, r)
-end
-
-
-function timeout(n::Int, v::Bool = false)
-    sleep(n)
-    v && println("Timed out")
-    return false
 end
 
 
@@ -72,10 +66,13 @@ function handle(
         "finished"
     ]
     server_state = states[1]
-    confirm = UInt8[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-    error   = UInt8[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    confirmcode  = UInt8[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+    errorcode = UInt8[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    maxtries = 5
 
+    tries = 0
     while server_state == states[1]  # waiting for file
+        tries += 1
         open(file, "r") do io
             # converts the UInt64 hash to an array of UInt8 partial hashes.
             h = reinterpret(UInt8, [hash(read(io, String))])
@@ -89,38 +86,53 @@ function handle(
                 bs = read(io, numbytes)
             end
         end
-        send(socket, server.host, server.port, confirm)
-        ip, r = receive_from(socket, server, recv_channels, error, 0.5)
-        r == confirm ? server_state = states[2] : println("Error while sending file to $server, retrying...")
+        send(socket, server.host, server.port, confirmcode)
+        ip, r = receive_from(socket, server, recv_channels, errorcode, 0.5)
+        if r == confirmcode
+            server_state = states[2]
+        else
+            tries > maxtries && return false, server  # just give up talking to this server at this point.
+            println("Error while sending file to $server, retrying ($(tries/maxtries))...")
+        end
     end
 
+    tries = 0
     chunk = take!(chunks)
     while server_state == states[2]  # waiting for job
-        send(socket, server.host, server.port, reinterpret(UInt8, [chunk.start, chunk.stop]))
+        tries += 1
 
-        ip, r = receive_from(socket, server, recv_channels, error, 0.5)
-        if r == confirm
+        send(socket, server.host, server.port, reinterpret(UInt8, [chunk.start, chunk.stop]))
+        ip, r = receive_from(socket, server, recv_channels, errorcode, 0.5)
+        if r == confirmcode
             server_state = states[3]
         else
             put!(chunks, chunk)
-            println("Error while sending job to $server, retrying...")
+            println("Error while sending job to $server, retrying ($(tries/maxtries))...")
+            tries > maxtries && return false, server  # just give up talking to this server at this point.
             sleep(0.1)  # wait some time to give other tasks a chance to take that chunk.
         end
     end
 
+    tries = 0
     r = UInt8[]
     while server_state == states[3]  # processing job
-        ip, r = receive_from(socket, server, recv_channels, error, 0.5)
-        if length(r) == chunk.stop - chunk.start + 1  # if the length of what we got is shorter/longer than what we expected
-            send(socket, server.host, server.port, error)
+        tries += 1
+
+        ip, r = receive_from(socket, server, recv_channels, errorcode, 0.5)
+        # if the length of what we got is shorter/longer than what we expected,
+        # or if recv timed out
+        # if we've tried too many times, send chunk back to chunks and return
+        if length(r) != chunk.stop - chunk.start + 1  || r == errorcode
+            tries > maxtries && put!(chunks, chunk); return false, server
+            send(socket, server.host, server.port, errorcode)
         else
-            send(socket, server.host, server.port, confirm)
+            send(socket, server.host, server.port, confirmcode)
             server_state = states[4]
         end
     end
 
     if server_state == states[4]  # finished
-        return server, r
+        return true, server, chunk, r
     end
 end
 
@@ -136,8 +148,8 @@ function main()
     numpixels = floor(Int, numbytes/3)  # Number of pixels per chunk
 
     view, scene = parseFile(file)
-    final = zeros(view.height, view.width, 3)
-    N = view.width * view.height  # Total number of pixels in image
+    final_image = Array{Union{UInt8, Missing}}(missing, view.height, view.width, 3)
+    N = view.height * view.width  # Total number of pixels in image
 
     chunks = Channel{UnitRange{Int64}}(ceil(Int, N/numpixels)) do ch  # Tracks pixel chunks waiting for from servers
         for chunk in [i+1:i+numpixels for i in 0:numpixels:N]
@@ -151,18 +163,21 @@ function main()
         server_list
     )
     tasklist = [(:take, t()) for t in tasks]
-    # TODO: create empty image, update it, know when to break the below loop when the image is finished, write the image ppm file
-    while true  # while image is not completed
+    # TODO: create empty image (done), update it (done), know when to break the below loop when the image is finished (done), write the image ppm file
+    while any(ismissing, final_image)  # while image is not completed
         i, r = select(tasklist)  # i is index of server_list, r is return value of first function to return
-        # depending on the value of r
-        if r == false  # timed out
-            println("Timed out in main execution.")
+        @assert length(r) >= 2  # r should be at least (success_code, server), with more if its successful
+
+        if r[1] == false  # timed out
+            println("Timed out in main execution with server $(r[2]). All retries should already have been attempted.")
         else
+            flag, server, chunk, result = r
+            final_image[chunk] = result  # this will not work - TODO: think about how best to define chunk, and how the bytes in result should be ordered.
             tasklist[i] = (:take, Task(() -> handle(socket, server_list[i], file, numbytes, chunks, recv_channels)))
         end
     end
-
     close(socket)
+
 end
 
 main()
