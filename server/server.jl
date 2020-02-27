1d86adbfc3956611f95c38c01a1477b00740dce9
jonathan-durbin <jongdurbin@gmail.com>
2020-02-26 22:29:43 -0500

    Make server.jl work.
    Wrap everything in a module, so `JULIA_DEBUG=Server julia server.jl` works
    Remove port from Client type
    Send to a known port, send from a random available port
    Add scene_dict to help with multiple clients.
    Remove extra comments
    Switch @info to @debug

--- server/server.jl
+++ server/server.jl
@@ -1,3 +1,5 @@
+module Server
+
 using Sockets
 import Base.Threads.@spawn
 
@@ -10,19 +12,19 @@ using .Select
 
 struct Client
     host::IPv4
-    port::Int
 end
+
 
 function receive_from(
         recv_socket::UDPSocket,
-        recv_channels::Dict{Client,Channel{Any}},
+        recv_channels::Dict{Client,Channel{Array{UInt8,1}}},
         clientlist::Array{Client,1}
     )
     resp = recvfrom(recv_socket)
-    client, r = Client(resp[1].host, resp[1].port), resp[2]
+    client, r = Client(resp[1].host), resp[2]
     @debug "Got $(length(r)) bytes from $client."
     if !(client in clientlist)
-        recv_channels[client] = Channel(Inf)
+        recv_channels[client] = Channel{Array{UInt8,1}}(Inf)
     end
     # if we receive from a client, then put the data in the channel for the corresponding client
     put!(recv_channels[client], r)
@@ -32,18 +34,20 @@ end
 
 function send_to(
         send_socket::UDPSocket,
-        send_channel::Channel{Tuple{Client, Array{UInt8,1}}}
+        send_channel::Channel{Tuple{Client, Array{UInt8,1}}},
+        send_to_port::Int
     )
     client, to_send = take!(send_channel)
-    send(send_socket, client.host, client.port, to_send)
+    send(send_socket, client.host, send_to_port, to_send)
     return client
 end
 
 
 function handle(
         client::Client,
-        recv_channels::Dict{Client,Channel{Any}},
-        send_channel::Channel{Tuple{Client, Array{UInt8,1}}}
+        recv_channels::Dict{Client,Channel{Array{UInt8,1}}},
+        send_channel::Channel{Tuple{Client, Array{UInt8,1}}},
+        scene_dict::Dict{Client, Array{Any, 1}}
     )
     states = [
         "waiting for file",
@@ -55,45 +59,33 @@ function handle(
 
     confirmcode = UInt8[0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
     errorcode   = UInt8[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-
-    scenefile = ""
-    scenehash = nothing
-
     done = false
-    debug = false
 
-    @info "Ready"
+    scene_dict[client] = ["", nothing]  # scene file, scene hash
+
+    @info "Handling $client."
     while !done
-        # TODO Test if correct client; add temporary bias to receive from previous client - might be done
-        # follow up on above TODO: create a dictionary like client -> scenefile, checking that each file is complete against its hash,
-        # based on what client the server is receiving from.
-        while scenehash != hash(scenefile) && server_state == states[1]  # waiting for file
-            @info "$server_state"
+        while hash(scene_dict[client][1]) != scene_dict[client][2] && server_state == states[1]  # waiting for file
+            @debug "$server_state, $client"
 
             r = take!(recv_channels[client])
-            # println("Firstclient == client? $(firstclient == client). Got $(length(r)) bytes. Scenehash is $scenehash, scenefile hash is $(hash(scenefile)).")
-            # client != firstclient && (send(sock, client.host, client.port, errorcode); println("Heard from a different client than expected, continuing.."); continue)
-            # println("Got $(length(r)) bytes from $client. Firstclient is $firstclient. Scenehash is $scenehash, scenefile hash is $(hash(scenefile)).")
             if r == confirmcode  # if r is confirm and the scene file isn't complete yet, send error and restart
-                @info "Got confirm code before expected, retrying..."
+                @error "Got confirm code before expected, retrying..."
                 put!(send_channel, (client, errorcode))
-                scenefile = ""
-                scenehash = nothing
-                client = ()
+                scene_dict[client] = ["", nothing]
                 continue
             end
-
-            scenehash = reinterpret(UInt64, r[1:8])[1]  # reinterpret combines an array of UInt8 to a single UInt64
-            scenefile *= String(copy(r[9:end]))
-            scenehash == hash(scenefile) && put!(send_channel, (client, confirmcode))  # got the whole file at this point
-            scenehash == hash(scenefile) && (server_state = states[2])
+            scene_dict[client][2] = reinterpret(UInt64, r[1:8])[1]  # reinterpret combines an array of UInt8 to a single UInt64
+            scene_dict[client][1] *= String(copy(r[9:end]))
+            scene_dict[client][2] == hash(scene_dict[client][1]) && put!(send_channel, (client, confirmcode))  # got the whole file at this point
+            scene_dict[client][2] == hash(scene_dict[client][1]) && (server_state = states[2])
         end
 
-        view, scene = parseFile(IOBuffer(scenefile))
+        view, scene = parseFile(IOBuffer(scene_dict[client][1]))
 
         chunk = 0:0
         while server_state == states[2]  # waiting for job
-            @info "$server_state"
+            @debug "$server_state, $client"
             r = take!(recv_channels[client])
             length(r) != 16 && (@error "error in state 2 length(r)=$(length(r))"; put!(send_channel, (client, errorcode)); continue)
             r = reinterpret(Int64, r)
@@ -104,7 +96,7 @@ function handle(
 
         @info "Rendering $chunk for $client."
         bmp = collect(Iterators.flatten(eachrow(render(view, scene, chunk))))  # this can be directly sent.
-        @info "Done, sending..."
+        @debug "Done, sending..."
         while server_state == states[3]  # processing job
             put!(send_channel, (client, bmp))
             r = take!(recv_channels[client])
@@ -112,7 +104,7 @@ function handle(
         end
 
         if server_state == states[4]  # finished
-            @info "$server_state"
+            @debug "$server_state, $client"
             server_state = states[2]
         end
     end
@@ -121,33 +113,34 @@ end
 
 function main()
     send_sock = UDPSocket()
-    bind(send_sock, ip"127.0.0.1", 9105)
+    # bind(send_sock, ip"127.0.0.1", 8105)
     recv_sock = UDPSocket()
-    bind(recv_sock, ip"127.0.0.1", 7105)
+    bind(recv_sock, ip"127.0.0.1", 9105)
+    send_to_port = 6105
 
-    recv_channels = Dict{Client,Channel{Any}}()
+    recv_channels = Dict{Client,Channel{Array{UInt8,1}}}()
     send_channel = Channel{Tuple{Client, Array{UInt8,1}}}(Inf)
+    scene_dict = Dict{Client, Array{Any,1}}()
     clientlist = Client[]
 
     r = @async receive_from(recv_sock, recv_channels, clientlist)
-    s = @async send_to(send_sock, send_channel)
+    s = @async send_to(send_sock, send_channel, send_to_port)
 
-    @info "Ready."
+    @info "Main loop starting."
     while true
         @select begin
             r |> client => begin
-                @info "Received from $client."
+                @debug "Received from $client."
                 if !(client in clientlist)
-                    @info "Now handling new client $client."
-                    # send_channel[client] = Channel(Inf)
-                    @spawn handle(client, recv_channels, send_channel)
+                    @info "Now handling new $client."
+                    @spawn handle(client, recv_channels, send_channel, scene_dict)
                     push!(clientlist, client)
                 end
                 r = @async receive_from(recv_sock, recv_channels, clientlist)
             end
             s |> client => begin
-                @info "Sent to $client."
-                s = @async send_to(send_sock, send_channel)
+                @debug "Sent to $client."
+                s = @async send_to(send_sock, send_channel, send_to_port)
             end
         end
     end
@@ -157,3 +150,5 @@ end
 
 
 main()
+
+end  # module Server
